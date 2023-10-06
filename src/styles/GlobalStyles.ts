459f77878beeff3ea9c126d16ddb9f2657cd126d
Jon Mico <jonmico@gmail.com>
2023-10-06 12:21:24 -0400

    Configure GlobalStyles.

---
+++ src/styles/GlobalStyles.ts
@@ -0,0 +1,26 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyles = createGlobalStyle`
+  :root {
+    --color-primary: hsl(4, 100%, 67%);
+    --color-dark-slate-grey: hsl(234, 29%, 20%);
+    --color-charcoal-grey: hsl(235, 18%, 26%);
+    --color-grey: hsl(231, 7%, 60%);
+    --color-white: hsl(0, 0%, 100%);
+  }
+
+  *,
+  *::before,
+  *::after {
+    box-sizing: border-box;
+    padding: 0;
+    margin: 0;
+  }
+
+  body {
+    font-family: 'Roboto', sans-serif;
+    color: var(--color-charcoal-grey)
+  }
+`;
+
+export default GlobalStyles;
