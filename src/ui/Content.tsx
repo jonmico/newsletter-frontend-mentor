c00ed67f962502e5a52dc13634467e486b611f20
Jon Mico <jonmico@gmail.com>
2023-10-09 23:32:11 -0400

    Work on some padding stuff at the bottom of the container.

--- src/ui/Content.tsx
+++ src/ui/Content.tsx
@@ -9,15 +9,15 @@ const StyledContent = styled.div`
   padding: 0 4rem 0 2rem;
   display: flex;
   flex-direction: column;
-  gap: 1.75rem;
+  gap: 1.5rem;
 
   @media only screen and (max-width: 950px) {
     gap: 1rem;
   }
 
   @media only screen and (max-width: 675px) {
-    padding: 2rem 1.5rem 1rem 1.5rem;
-    gap: 1.75rem;
+    padding: 2rem 1.5rem 2.5rem 1.5rem;
+    gap: 1.5rem;
   }
 `;
 
